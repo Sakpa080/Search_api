96cce3d60c5d406384358d86a878404470cca5f5
nathanieluriri <uririnathaniel@gmail.com>
2024-12-21 22:53:51 +0100

    added Ai stuff

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ load_dotenv()
 GOOGLE_APIKEY = os.getenv('G_API')
 SEARCHID=os.getenv('S_ID')
 BASE_URL = os.getenv('BASE_URL')
+OpenAI_API_KEY = os.getenv("OAK")
 
 from openai import OpenAI
-client = OpenAI()
+client = OpenAI(api_key=OpenAI_API_KEY)
 
 
 # Define the initial messages array
@@ -136,8 +137,42 @@ st.write("# Brand Ambassador Finder")
 
 
 
-st.selectbox(label="where is your brand based in?",options=["Nigeria","London","New york"],key="brand_location")
-st.selectbox(label="What category or industry is your brand focused on?",options=["Pet care","Security Services", "Luxury shoes", "Bespoke interior Designs"],key="brand_category")
+st.selectbox(label="where is your brand based in?",options=[
+    "Nigeria", "London", "New York", "Tokyo", "Paris", "Sydney", "Beijing", "Toronto", "Mumbai", "Dubai",
+    "Berlin", "Cape Town", "Buenos Aires", "Moscow", "Cairo", "Seoul", "Bangkok", "Mexico City", "Jakarta", "Istanbul",
+    "Rio de Janeiro", "Lagos", "Singapore", "Los Angeles", "Kuala Lumpur", "Rome", "Shanghai", "Melbourne", "Hong Kong", "Madrid",
+    "Dublin", "Nairobi", "Lima", "Vienna", "Athens", "Manila", "Karachi", "Dhaka", "Abu Dhabi", "Amsterdam",
+    "Chicago", "Bogotá", "Riyadh", "Santiago", "Barcelona", "Colombo", "Ankara", "Brisbane", "Kiev", "Warsaw",
+    "San Francisco", "Doha", "Edinburgh", "Helsinki", "Oslo", "Stockholm", "Copenhagen", "Lisbon", "Vancouver", "Montreal",
+    "Zurich", "Geneva", "Prague", "Budapest", "Brussels", "Hanoi", "Phnom Penh", "Kathmandu", "Islamabad", "Jakarta",
+    "Canberra", "Addis Ababa", "Accra", "Casablanca", "Tunis", "Rabat", "Algiers", "Amman", "Tehran", "Baghdad",
+    "Ho Chi Minh City", "Taipei", "Macau", "Perth", "Adelaide", "Darwin", "Christchurch", "Auckland", "Wellington", "Hobart",
+    "Bangalore", "Hyderabad", "Pune", "Chennai", "Ahmedabad", "Surat", "Lucknow", "Patna", "Kolkata", "Jaipur"
+]
+,key="brand_location")
+st.selectbox(label="What category or industry is your brand focused on?",options=[
+    "Pet Care", "Security Services", "Luxury Shoes", "Bespoke Interior Designs", "Digital Marketing", "Fitness Coaching", 
+    "Event Planning", "Handmade Jewelry", "Organic Farming", "Online Education", 
+    "Photography Services", "Custom Furniture", "Tech Support", "Mobile App Development", "Virtual Assistant Services", 
+    "Health and Wellness Products", "Eco-Friendly Packaging", "Financial Consulting", "Social Media Management", "Car Rental Services", 
+    "Home Cleaning Services", "Landscaping", "Meal Prep Services", "Luxury Handbags", "Clothing Boutique", 
+    "Graphic Design", "Tutoring Services", "Real Estate Development", "E-commerce Store", "Hair and Beauty Salon", 
+    "Wedding Planning", "Drone Videography", "Food Truck", "Specialty Coffee", "Art Gallery", 
+    "Catering Services", "Craft Supplies", "Mobile Car Wash", "Subscription Boxes", "Sustainable Fashion", 
+    "IT Consulting", "Yoga Studio", "Auto Repair Shop", "Travel Agency", "Baking and Pastries", 
+    "Bookstore", "Music Lessons", "Gift Wrapping Services", "Daycare Center", "Personal Training", 
+    "Podcast Production", "Career Coaching", "Online Reselling", "Personal Styling", "Freelance Writing", 
+    "Translation Services", "Gaming Lounge", "Interior Painting", "Renewable Energy Solutions", "Custom T-Shirts", 
+    "Virtual Reality Experiences", "Public Relations Agency", "Coffee Roasting", "Candle Making", "Antique Restoration", 
+    "Mobile Massage Therapy", "Life Coaching", "Childproofing Services", "Shoe Repair", "Toy Manufacturing", 
+    "Pet Grooming", "Health Food Store", "Fishing Gear", "Gourmet Popcorn", "Customized Stationery", 
+    "Self-Defense Classes", "Home Staging", "Party Supplies", "Computer Repair", "Sports Coaching", 
+    "Mobile Notary Services", "Greenhouse Installation", "Car Detailing", "Solar Panel Installation", "Corporate Training", 
+    "Video Editing Services", "Scented Soaps", "Woodworking", "Bed and Breakfast", "Sports Equipment Rental", 
+    "Organic Skincare", "Wedding Photography", "Custom Cake Design", "Online Fitness Programs", "Property Management", 
+    "SEO Services", "Virtual Event Hosting", "3D Printing Services", "Luxury Watches", "Tailoring and Alterations"
+]
+,key="brand_category")
 st.text_input(label="What values or traits are most important to your brand when choosing an ambassador?(optional)",key="extra")
 if st.button("Generate Search query"):
     new_user_input = {
@@ -145,7 +180,7 @@ if st.button("Generate Search query"):
         "content": [
             {
                 "type": "text",
-                "text": f"I'm looking for someone to represent my brand help me write a search query for google to make it possible to find a brand ambassador my brand is of a {st.session_state.brand_category} category and the business is based in {st.session_state.brand_location} extra information--{st.session_state.extra}"
+                "text": f"I'm looking for someone to represent my brand help me write a search query for google to make it possible to find a brand ambassador my brand is of a {st.session_state.brand_category} category and the business is based in {st.session_state.brand_location} important values and traits to the brand--{st.session_state.extra}"
             }
         ]
     }
@@ -165,12 +200,12 @@ if st.button("Generate Search query"):
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
-    )
+    )   
 
     # Extract and print the assistant's response
-    assistant_response = response["choices"][0]["message"]["content"]
+    print(response.choices[0].message.content)
+    assistant_response = response.choices[0].message.content
     st.session_state.search_query= assistant_response
-print(assistant_response)
 
 st.text_input("what are you looking for ?",key="search_query",disabled=True)
 st.button("Search",on_click=search_func)
@@ -184,9 +219,11 @@ if st.session_state.images:
             image = Image.open(BytesIO(response.content))
             # Display image
             with tab:
+                st.code(st.session_state.summaries[url].get('username'))
                 st.image(image, caption=f"@{ st.session_state.summaries[url].get('username')}\'s Profile Picture ")
                 st.link_button(type='secondary',label=f"Click me to go to @{st.session_state.summaries[url].get('username')}'s instagram page ",url=f"{st.session_state.summaries[url].get('profileLink')}")
                 st.info(f"Bio: { st.session_state.summaries[url].get('biography')}",icon="🥇")
+                st.code(st.session_state.summaries[url].get('username'))
                 st.write(f"Full Name :red-background[{ st.session_state.summaries[url].get('fullName')}]")
                 st.write(f"Username @:blue-background[{ st.session_state.summaries[url].get('username')}]")
                 st.write(f"Number of People Following @{st.session_state.summaries[url].get('username')} :green-background[{ st.session_state.summaries[url].get('No_of_followers')}]")
@@ -202,7 +239,7 @@ if st.session_state.images:
                 pass
             else:
                 st.warning(f"something went wrong {e}")
-st.markdown("[Go to Top Button](#brand-ambassador-finder)")
+st.markdown("[tap me to Go to the Top](#brand-ambassador-finder)")
 
 st.markdown('''
     <style>
